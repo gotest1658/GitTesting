498e80a8a2058608227e4e36f94dd711bacbddb2
Pierre <pierre1688@gmail.com>
2019-07-14 11:36:54 +0800

    The first init

---
+++ app/src/main/java/tw/idv/test/gittesting/MainActivity.java
@@ -0,0 +1,13 @@
+package tw.idv.test.gittesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
